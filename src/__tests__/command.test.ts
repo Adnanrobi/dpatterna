244a15911e06cbcf18ccc18e343e8398e2789123
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-08-17 22:58:45 +0600

    created test cases for the light

---
+++ src/__tests__/command.test.ts
@@ -0,0 +1,31 @@
+import { Light, LightOnCommand, LightOffCommand, RedLightOnCommand, RedLightDecreaseCommand, RedLightIncreaseCommand } from "../patterns/command/command-remote";
+
+describe('Command Pattern Test', () => {
+    test('Turn On Light', () => {
+        let expected = new Light();
+        expected.isLightOn = true;
+        let reality = new LightOnCommand(expected);
+        expect(expected.on()).toEqual(reality.execute());
+    })
+    test('Turn Off Light', () => {
+        let expected = new Light();
+        let reality = new LightOffCommand(expected);
+        expect(expected.off()).toEqual(reality.execute());
+    })
+    test('Red Light Increased', () => {
+        let expected = new Light();
+        expected.isLightOn = true;
+        expected.isRedLightOn=true;
+        expected.luminosity=1;
+        let reality = new RedLightIncreaseCommand(expected);
+        expect(expected.redTwo()).toEqual(reality.execute());
+    })
+    test('Red Light Decreased', () => {
+        let expected = new Light();
+        expected.isLightOn = true;
+        expected.isRedLightOn=true;
+        expected.luminosity=1;
+        let reality = new RedLightDecreaseCommand (expected);
+        expect(expected.redZero()).toEqual(reality.execute());
+    })  
+})
